ba9a4e96ffe058cc4e08b9ab429ed20a426a8a84
Giuseppe Viviani <giuseppe_viviani@hotmail.it>
2017-04-28 16:31:21 +0200

    Update Trail - Testing the services

---
+++ custom/cuppytrail/src/de/hybris/platform/cuppytrail/StadiumService.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package de.hybris.platform.cuppytrail;
+
+import de.hybris.platform.cuppytrail.model.StadiumModel;
+
+import java.util.List;
+
+
+public interface StadiumService
+{
+
+	List<StadiumModel> getStadiums();
+
+
+	StadiumModel getStadiumForCode(String code);
+
+}
